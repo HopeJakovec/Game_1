2a65149a208f486b21dace3c2c4175328783f956
Hope Jakovec <hope.jakovec@gmail.com>
2023-04-23 11:02:36 +0300

    added animation for every element Tree

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,19 @@
     let isPause = false;
     let animationId = null;
 
-    const speed = 3;
+    const speed = 5;
     
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.trees');
 
-    const tree1 = trees[0];
-    const coordsTree1 = getCoords(tree1);
+    const treesCoords = [];
+
+    for (let i = 0; i < trees.length; i++) {
+        const tree = trees[i];
+        const coordsTree = getCoords(tree);
+
+        treesCoords.push(coordsTree);
+    }
 
     animationId = requestAnimationFrame(startGame);
 
@@ -19,13 +25,23 @@
     };
 
     function treesAnimation() {
-        const newCoordY = coordsTree1.y + speed;
-        coordsTree1.y = newCoordY;
-        tree1.style.transform = `translate(${coordsTree1.x}px, ${newCoordY}px)`;
+        for (let i = 0; i < trees.length; i++) {
+            const tree = trees[i];
+            const coords = treesCoords[i];
+
+            let newYCoord = coords.y + speed;
+
+            if (newYCoord > window.innerHeight) {
+                newYCoord = -tree.height;
+            }
+    
+            treesCoords[i].y = newYCoord;
+            tree.style.transform = `translate(${coords.x}px, ${newYCoord}px)`;
+        }
     };
 
     function getCoords(element) {
-        const matrix = window.getComputedStyle(tree1).transform;
+        const matrix = window.getComputedStyle(element).transform;
         const array = matrix.split(',');
         const y = array[array.length - 1];
         const x = array[array.length - 2];
