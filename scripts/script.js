08132ddbe6ffecfa67cd3c9f2ee662a9437b2be5
Hope Jakovec <hope.jakovec@gmail.com>
2023-04-24 22:20:46 +0300

    added logic of control for the element Car

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,13 @@
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.trees');
 
+    const carCoords = getCoords(car);
+    const carMoveInfo = {
+        top: null,
+        bottom: null,
+        left: null,
+        right: null,
+    }
     const treesCoords = [];
 
     for (let i = 0; i < trees.length; i++) {
@@ -14,6 +21,70 @@
         const coordsTree = getCoords(tree);
 
         treesCoords.push(coordsTree);
+    }
+
+    document.addEventListener('keydown', (event) => {
+        const code = event.code;
+
+        if (code === 'ArrowUp' && carMoveInfo.top === null) {
+            carMoveInfo.top = requestAnimationFrame(carMoveToTop);
+        } else if (code === 'ArrowDown' && carMoveInfo.bottom === null) {
+            carMoveInfo.bottom = requestAnimationFrame(carMoveToBottom);
+        } else if (code === 'ArrowLeft' && carMoveInfo.left === null) {
+            carMoveInfo.left = requestAnimationFrame(carMoveToLeft);
+        } else if (code === 'ArrowRight' && carMoveInfo.right === null) {
+            carMoveInfo.right = requestAnimationFrame(carMoveToRight);
+        } 
+    })
+
+    document.addEventListener('keyup', (event) => {
+        const code = event.code;
+
+        if (code === 'ArrowUp') {
+            cancelAnimationFrame(carMoveInfo.top);
+            carMoveInfo.top = null;
+        } else if (code === 'ArrowDown') {
+            cancelAnimationFrame(carMoveInfo.bottom);
+            carMoveInfo.bottom = null;
+        } else if (code === 'ArrowLeft') {
+            cancelAnimationFrame(carMoveInfo.length);
+            carMoveInfo.length = null;
+        } else if (code === 'ArrowRight') {
+            cancelAnimationFrame(carMoveInfo.right);
+            carMoveInfo.right = null;
+        } 
+    })
+
+    function carMoveToTop() {
+        const newY = carCoords.y - 5;
+        carCoords.y = newY;
+        carMove(carCoords.x, newY);
+        carMoveInfo.top = requestAnimationFrame(carMoveToTop);
+    }
+
+    function carMoveToBottom() {
+        const newY = carCoords.y + 5;
+        carCoords.y = newY;
+        carMove(carCoords.x, newY);
+        carMoveInfo.bottom = requestAnimationFrame(carMoveToBottom);
+    }
+
+    function carMoveToLeft() {
+        const newX = carCoords.x - 5;
+        carCoords.x = newX;
+        carMove(newX, carCoords.y);
+        carMoveInfo.left = requestAnimationFrame(carMoveToLeft);
+    }
+
+    function carMoveToRight() {
+        const newX = carCoords.x + 5;
+        carCoords.x = newX;
+        carMove(newX, carCoords.y);
+        carMoveInfo.right = requestAnimationFrame(carMoveToRight);
+    }
+
+    function carMove(x, y) {
+        car.style.transform = `translate(${x}px, ${y}px)`;
     }
 
     animationId = requestAnimationFrame(startGame);
